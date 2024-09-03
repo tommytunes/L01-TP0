324f1dfc2e79c65550d90c06f227a1a1ccc3752e
Thomas Staples <thomasstaples@Thomass-MacBook-Pro.local>
2024-09-03 17:22:01 -0400

    rajouter mon nom

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
+from cmath import pi
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+print((rayon**2)*pi)
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Staples Thomas")
